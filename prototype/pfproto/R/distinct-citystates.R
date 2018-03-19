b14e40ac2ac1a9bbb6d1d673fd80cb75f21e2591
Chris Baker <chriscrewbaker@gmail.com>
2018-03-19 10:10:17 -0400

    add distinct citystate code

---
+++ prototype/pfproto/R/distinct-citystates.R
@@ -0,0 +1,21 @@
+distinct_citystates <- function(citystate_centers) {
+  lapply(citystate_centers, nrby_citystates) %>%
+    do.call(c, .) %>%
+    unique()
+}
+
+citystate_centers <- c(
+  "washington, dc",
+  "new york, ny"
+)
+
+nrby_citystates <- function(citystate) {
+  p_find(location = citystate, count = 1000) %>%
+    as_pftibble() %>%
+    select(city, state, zip) %>%
+    mutate(citystate = as_citystate(city, state)) %>%
+    pull("citystate") %>%
+    unique()
+}
+
+as_citystate <- function(city, state) sprintf("%s, %s", city, state)
