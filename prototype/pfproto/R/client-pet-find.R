4ff4d8cfecc383e4767ac3dbe9245a2b08739fef
Chris Baker <chriscrewbaker@gmail.com>
2018-03-23 17:53:03 -0400

    make sure pftibble obj has similar schema as sql db

--- prototype/pfproto/R/client-pet-find.R
+++ prototype/pfproto/R/client-pet-find.R
@@ -3,12 +3,9 @@ p_find <- function(pf_key = get_pf_key(),
                    output = "full",
                    count = 100,
                    offset = 1) {
-  location <- ifelse(
-    is.character(location), utils::URLencode(location), location
-  )
   paste0(
     "http://api.petfinder.com/pet.find?key=", pf_key,
-    "&location=", location,
+    "&location=", utils::URLencode(location),
     "&output=", output,
     "&count=", count,
     "&offset=", offset,
@@ -24,23 +21,28 @@ as_pftibble <- function(p_find_out) {
   pet <- p_find_out$petfinder$pets$pet
 
   tibble(
-    id = pet$id$`$t`,
-    shelter_pet_id = pet$shelterPetId$`$t`,
-    name = pet$name$`$t`,
+    # fields in "pet" table
+    pet_id = as.numeric(pet$id$`$t`),
     shelter_id = pet$shelterId$`$t`,
-    last_update = lubridate::as_datetime(pet$lastUpdate$`$t`),
-    option = lapply(pet$options$option, as_vec),
+    name = pet$name$`$t`,
     status = pet$status$`$t`,
-    city = pet$contact$city$`$t`,
-    state = pet$contact$state$`$t`,
-    zip = as.numeric(pet$contact$zip$`$t`),
     age = pet$age$`$t`,
     size = pet$size$`$t`,
-    breed = lapply(pet$breeds$breed, as_vec),
     sex = pet$sex$`$t`,
     mix = pet$mix$`$t`,
     description = pet$description$`$t`,
-    photo = lapply(pet$media$photos$photo, null_to_na)
+
+    # fields that create pet_(option|breed|photo) tables
+    option = lapply(pet$options$option, as_vec),
+    breed = lapply(pet$breeds$breed, as_vec),
+    photo = lapply(pet$media$photos$photo, null_to_na),
+
+    last_update = lubridate::as_datetime(pet$lastUpdate$`$t`),
+
+    # shelter fields
+    city = pet$contact$city$`$t`,
+    state = pet$contact$state$`$t`,
+    zip = pet$contact$zip$`$t`
   ) %>%
     add_class("pftibble")
 }
