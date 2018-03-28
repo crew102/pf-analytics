e2a413febdedbf2dbea7e9559c2dbea047f52a21
christopher crew baker <chriscrewbaker@gmail.com>
2018-03-27 21:08:16 -0400

    move basic test parser into Model1TestParser class

---
+++ pf-analytics/client/src/main/java/io/github/crew102/clients/Model1TestParser.java
@@ -0,0 +1,18 @@
+package io.github.crew102.clients;
+
+import java.io.IOException;
+import com.google.gson.Gson;
+import io.github.crew102.model1.apimethods.FindPets;
+
+public class Model1TestParser {
+	
+	/*
+	public static void main(String[] args) throws IOException {
+		Utils.catDir();
+		Gson gson = new Gson();  
+		String json = Utils.readJSONfile("../../prototype/pfproto/inst/dev/ex-json.json");
+		FindPets findpets = gson.fromJson(json, FindPets.class);
+	}
+	*/
+	
+}
