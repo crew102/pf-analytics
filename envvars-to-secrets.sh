5f0879993327e939165a97af64f98c81444da625
christopher crew baker <chriscrewbaker@gmail.com>
2018-04-12 00:29:50 -0400

    add script to write envvars to secrets dir for travis build

---
+++ envvars-to-secrets.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# secrets that are stored as env vars on travis
+secrets=(MYSQL_ROOT_PASSWORD AUTH0_CLIENT_SECRET AUTH0_CLIENT_ID COOKIE_SECRET \
+PF_KEY RSTUDIO_CBAKER_PASSWORD)
+
+for i in ${secrets[*]}
+do
+val=$(eval echo "$""${i}")
+echo $val > `pwd`/services/secrets/"$i"
+done
