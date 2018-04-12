e793d57fe5384233d186254dd09119cf7a73c17c
Chris Baker <chriscrewbaker@gmail.com>
2018-04-12 00:11:51 -0400

    add method to read secrets

--- pf-analytics/client/src/main/java/io/github/crew102/clients/Utils.java
+++ pf-analytics/client/src/main/java/io/github/crew102/clients/Utils.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
+import io.github.crew102.exceptions.SecretNotFoundException;
 
 public final class Utils {
   
@@ -29,5 +30,23 @@ public final class Utils {
     String dir = Paths.get(".").toAbsolutePath().normalize().toString();
     System.out.println(dir);
   }
-
+  
+  public static String getSecret(String secret) throws SecretNotFoundException, IOException {
+    
+    String envvar = System.getenv(secret);
+    String localFile = "../../services/secrets/" + secret;
+    String dcFile = "/run/secrets/" + secret.toLowerCase().replaceAll("_", "-");
+    
+    if (envvar != null) {
+      return envvar;
+    } else if (new File(localFile).exists()) {
+      return readFile(localFile);
+    } else if (new File(dcFile).exists()) {
+      return readFile(dcFile);
+    } else {
+      String exp = "Could not find secret: " + secret;
+      throw new SecretNotFoundException(exp);
+    }
+  }
+  
 }
---
+++ pf-analytics/client/src/main/java/io/github/crew102/exceptions/SecretNotFoundException.java
@@ -0,0 +1,9 @@
+package io.github.crew102.exceptions;
+
+public class SecretNotFoundException extends Exception {
+  
+  public SecretNotFoundException(String message) {
+    super(message);
+  }
+
+}
