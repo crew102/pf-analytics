28b5b96333723293247de85a4403140da0bc72fc
Chris Baker <chriscrewbaker@gmail.com>
2018-04-12 00:10:28 -0400

    drop first data model used for gson parsing

--- pf-analytics/model/src/main/java/io/github/crew102/model1/midlevel/PetFinder.java
+++
@@ -1,9 +0,0 @@
-package io.github.crew102.model1.midlevel;
-
-import io.github.crew102.model1.utils.StringWrapper;
-
-public class PetFinder {
-  StringWrapper lastOffset;
-  Pets pets;
-  Header header;
-}
--- pf-analytics/model/src/main/java/io/github/crew102/model1/pet/Breeds.java
+++
@@ -1,7 +0,0 @@
-package io.github.crew102.model1.pet;
-
-import io.github.crew102.model1.utils.StringWrapper;
-
-public class Breeds {
-  StringWrapper breed;
-}
