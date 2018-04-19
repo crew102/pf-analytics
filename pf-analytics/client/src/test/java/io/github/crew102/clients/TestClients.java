ca590a1ff86c20c5fc4b1042e081ccc1e8664c9f
Chris Baker <chriscrewbaker@gmail.com>
2018-04-19 16:40:51 -0400

    move client testing into test dir

--- pf-analytics/client/src/test/java/io/github/crew102/clients/TestClients.java
+++ pf-analytics/client/src/test/java/io/github/crew102/clients/TestClients.java
@@ -1,25 +1,30 @@
 package io.github.crew102.clients;
 
-import java.util.HashMap;
 import io.github.crew102.parsers.PfResultParser;
 import io.github.crew102.rawresponses.PetfinderPetRecords;
+import java.util.HashMap;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-public class FindPetsRequest {
+public class TestClients {
   
-  public static void main(String[] args) throws Exception {
+  @Test
+  public void testPetFindMethod() throws Exception {
     
     HashMap<String, String> arglist = new HashMap<>();
     arglist.put("animal", "dog");
-    arglist.put("count", "100");
+    arglist.put("count", "1000");
     arglist.put("output", "full");
     arglist.put("location", "20008");
     
-    HttpRequest apireq = new HttpRequest(arglist);
+    HttpRequest apireq = new HttpRequest("pet.find", arglist);
     String output = apireq.makeRequest();
     
     PfResultParser resparser = new PfResultParser();
     PetfinderPetRecords res = resparser.parse(output);
+    
+    assertEquals(1000, res.getPetList().size());
 
   }
-  
+
 }
