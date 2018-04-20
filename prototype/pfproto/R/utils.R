addb6a878c6d04d71f02c4fa5a1cecbf9fb353f6
christopher crew baker <chriscrewbaker@gmail.com>
2018-04-20 17:49:24 -0400

    map mysql server to 3307 on host to avoid doubling down on 3306 on travis

--- prototype/pfproto/R/utils.R
+++ prototype/pfproto/R/utils.R
@@ -39,7 +39,7 @@ create_pool <- function(host = "mysql", password = get_secret("MYSQL_ROOT_PASSWO
     drv = RMySQL::MySQL(),
     dbname = "pf_dev",
     host = host,
-    port = 3306,
+    port = 3307,
     username = "root",
     password = password
   )
