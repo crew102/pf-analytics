3b12e6cb8667760f7926c616629519e81d50c6c1
christopher crew baker <chriscrewbaker@gmail.com>
2018-04-26 02:21:29 -0400

    drop unused param

--- shiny-server/pfapp/inst/pfapp/www/js/access-token.js
+++ shiny-server/pfapp/inst/pfapp/www/js/access-token.js
@@ -1,4 +1,4 @@
-$(document).on('shiny:sessioninitialized', function(event) {
+$(document).on('shiny:sessioninitialized', function() {
   var access_token = localStorage.accessToken;
   Shiny.onInputChange("access_token", access_token);
 });
