2233e787a39cbad27165c9aa8731f19d1bc91d76
chris baker <chriscrewbaker@gmail.com>
2019-03-11 12:47:05 -0400

    add rewrite of pet tracking scriipt

--- prototype/pfproto/inst/dev/dev.R
+++ prototype/pfproto/inst/dev/dev.R
@@ -11,23 +11,66 @@ gen_db(pool)
 
 db_update <- function(today_data, today_day, pool) {
 
+  browser()
   dbtbls <- fetch_all_tables(pool)
 
-  # see if any of the pets in the pet_tracking are not in today's data..these
-  # are pets that have since been removed since we last updated, so we need to
-  # move them over from pet_tracking to pet_dof
-  removed_pets <- dbtbls$pet_tracking %>% anti_join(today_data, by = "pet_id")
-  dbtbls$pet_dof <- removed_pets %>%
-    mutate(dof = interval(first_seen, today_day) / days(1)) %>%
-    select(pet_id, dof)
-  copy_to2(pool, dbtbls$pet_dof, "pet_dof")
+  tracking_pet <- dbtbls$pet %>% filter(is.na(dof))
 
+  # we look for changed ids amongst pets we're tracking at have supposedly been
+  # removed from petfinder based on their ids. an id is considered changed if
+  # we're tracking it, its id can be found in today's data, but its shelter_id,
+  # name, age, size are all found in today's data
+  changed_ids <- tracking_pet %>%
+    anti_join(today_data, by = "pet_id") %>%
+    inner_join(today_data, by = c("shelter_id", "name", "age", "size")) %>%
+    select(pet_id.x, pet_id.y) %>%
+    rename(original_pet_id = pet_id.x, new_pet_id = pet_id.y)
+
+  if (nrow(changed_ids)) {
+    browser()
+  }
+
+  # update today data by subing in old ids for those pets that have a changed
+  # id
+  today_data <- today_data %>%
+    left_join(changed_ids, by = c("pet_id" = "new_pet_id")) %>%
+    mutate(pet_id = ifelse(is.na(original_pet_id), pet_id, original_pet_id)) %>%
+    select(-original_pet_id)
+
+  # ...now we can determine which pets were actually removed
+  removed_pets <- tracking_pet %>% anti_join(today_data, by = "pet_id")
+
+  pet1 <- dbtbls$pet %>%
+    mutate(
+      first_day_not_seen = if_else(
+        pet_id %in% removed_pets$pet_id & is.na(first_day_not_seen),
+        today_day, # add first_day_not_seen if it's the first time
+        first_day_not_seen
+      )
+    ) %>%
+    mutate(
+      days_since = if_else(
+        pet_id %in% removed_pets$pet_id & !is.na(first_day_not_seen),
+        interval(first_day_not_seen, today_day) / days(1),
+        0
+      )
+    ) %>%
+    mutate(
+      dof = if_else(
+        days_since >= 5,
+        interval(first_seen, today_day) / days(1),
+        as.double(dof)
+      )
+    ) %>%
+    select(-days_since)
+
+
+  # figue out what the new pets are and add them to pet1
   existing_pets <- tibble(
     pet_id = c(
       dbtbls$initial_pets$pet_id,
       dbtbls$blacklisted_pets$pet_id,
-      dbtbls$pet_tracking$pet_id,
-      dbtbls$pet_dof$pet_id
+      pet1$pet_id
     )
   )
 
@@ -36,7 +79,7 @@ db_update <- function(today_data, today_day, pool) {
       anti_join(existing_pets, by = "pet_id") %>%
       mutate(days_since_last_update = interval(last_update, today_day) / days(1)) %>%
       # a new pet must not have been seen before and have been recently added to pf (within last 2 days)
-      mutate(should_blacklist = abs(days_since_last_update) >= 2)
+      mutate(should_blacklist = abs(days_since_last_update) >= 5)
 
   # update blacklisted_pets
   # blackedlisted pets are those new pets (pet_ids we haven't seen before) that
@@ -52,37 +95,24 @@ db_update <- function(today_data, today_day, pool) {
   }
   copy_to2(pool, dbtbls$blacklisted_pets, "blacklisted_pets")
 
-  # update pet_tracking table to include the new pets from today
-  new_pets_to_track <- new_pets %>% filter(!should_blacklist)
-  have_new_tracking <- nrow(new_pets_to_track) != 0
-  if (have_new_tracking) {
-    dbtbls$pet_tracking <- new_pets_to_track %>%
-      mutate(first_seen = last_update) %>%
-      select(pet_id, first_seen) %>%
-      rbind(dbtbls$pet_tracking) # add to existing pet_tracking table
-    copy_to2(pool, dbtbls$pet_tracking, "pet_tracking")
-  }
+  # update pet to include the new pets from today
+  pet2 <- new_pets %>%
+    filter(!should_blacklist) %>%
+    rename(first_seen = last_update) %>%
+    select(pet_id, shelter_id, name, age, size, sex, first_seen) %>%
+    mutate(first_day_not_seen = as.POSIXct(NA), dof = NA) %>%
+    rbind(pet1)
 
-  # "pet" and "shelter" table update
-  if (have_new_tracking) {
-    dbtbls$pet <- rbind(dbtbls$pet, new_pets_to_track[, colnames(dbtbls$pet)])
-    dbtbls$shelter <-
-      today_data %>%
-        select(shelter_id, city, state, zip) %>%
-        rbind(dbtbls$shelter) %>% # what if shelter changes city/state/zip?
-        distinct()
-    copy_to2(pool, dbtbls$pet, "pet")
-    copy_to2(pool, dbtbls$shelter, "shelter")
-  }
+  copy_to2(pool, pet2, "pet")
 
   print(today_day)
 }
 
 if (is_first_day(pool)) {
   write_init_pets(readRDS(files_df$file[1]), pool)
-} else {
-  lapply(
-    1:35,
-    function(x) db_update(readRDS(files_df$file[x]), files_df$date[x], pool)
-  )
 }
+lapply(
+    2:100,
+    function(x) db_update(readRDS(files_df$file[x]), files_df$date[x], pool)
+)
+
--- prototype/pfproto/inst/sql/create-db.sql
+++ prototype/pfproto/inst/sql/create-db.sql
@@ -2,64 +2,22 @@ drop database if exists pf_dev;
 create database if not exists pf_dev;
 use pf_dev;
 
-# "Info" tables where rows get inserted but never altered
 create table if not exists pet (
   pet_id int not null,
   shelter_id varchar(10) not null,
   name varchar(256) default null,
-  status char(1) default null,
   age varchar(6) default null,
   size varchar(2) default null,
   sex char(1) default null,
-  mix varchar(3) default null,
-  description text default null
+  first_seen datetime not null,
+  first_day_not_seen datetime default null,
+  dof int default null
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
-create table if not exists pet_option (
-  pet_id int not null,
-  `option` varchar(20) not null
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
-create table if not exists pet_breed (
-  pet_id int not null,
-  breed varchar(50) not null
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
-create table if not exists pet_photo (
-  pet_id int not null,
-  size varchar(3) not null,
-  url varchar(255) default null,
-  photo_id int not null
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
-create table if not exists shelter (
-  shelter_id varchar(10) not null,
-  city varchar(256) default null,
-  state varchar(256) default null,
-  zip varchar(20) default null
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
-# "Analytics" tables used to track days on petfinder (dof)
 create table if not exists initial_pets (
   pet_id int not null
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 create table if not exists blacklisted_pets (
   pet_id int not null
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
-create table if not exists pet_tracking (
-  pet_id int not null,
-  first_seen datetime not null
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
-create table if not exists pet_dof (
-  pet_id int not null,
-  dof int not null
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
-create table if not exists shelter_activity (
-  shelter_id varchar(10) not null,
-  activity_date datetime not null,
-  activity_type char(1) not null
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
